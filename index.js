02dc5e1ec39cca6d60bb155fd359c6ee8827ee66
uyarefecan <efecanuyar@hotmail.com>
2018-08-30 13:07:24 +0300

    Added all descriptions

--- index.js
+++ index.js
@@ -231,7 +231,174 @@
     '&emsp;Otoparkımızda birçok bankaya ait bankamatik bulunmaktadır.<br><br>' +
     '&emsp;Hastanemiz Çanakkale-İzmir Karayoluüzerinde, Edremit’e 3.Km mesafededir.'
     ,
-    '1-giri': 'Description 2'
+    '1-giri': 'ÖZEL EDREMİT KÖRFEZ HASTANESİ HAKKINDA<br><br>' +
+    '&emsp;Kuzey Ege Bölgesinde sağlık alanında tüm branşlarda (dahili ve cerrahi ) ' +
+    'hissedilen eksiklikler nedeniyle planlanıp bölgenin gereksinimlerine uygun olarak 2010 ' +
+    'yılında hizmet vermeye başlayan A sınıfı bir hastane olarak çevre il ve ilçelerde başta ' +
+    'olmak üzere Balıkesir Edremit Körfez Bölgesinin en modern ve en kapsamlı sağlık kuruluşlarından biridir.<br><br>' +
+    '&emsp;Yaklaşık 11 bin metrekare kapalı alana kurulu olan hastanemiz; 28’i yoğun bakım olmak üzere toplam 93 hasta yatağı, ' +
+    'tam teşekküllü genel yoğun bakım, kardiyovasküler cerrahi yoğun bakım, koroner yoğun bakım, cerrahi yoğun bakım ve ' +
+    'yenidoğan yoğun bakım servisleri ve modern ameliyathaneleri, doğumhaneleri, fizik tedavi ve rehabilitasyon servisi, ' +
+    'diş ağız ve çene cerrahisi ünitesi, modern radyoloji görüntüleme merkezi, tıbbi biyokimya ve mikrobiyoloji laboratuvarları' +
+    'ile hemen hemen tüm branşlarda SGK, anlaşmalı özel sigorta ve kurumlarla poliklinik, yatarak teşhis ve tedavi hizmeti sunmaktadır.<br><br>' +
+    '&emsp;Hastanemizde tüm hasta ve misafirlerimizin kullanabileceği ücretsiz otoparklarımız hizmet vermektedir.<br><br>' +
+    '&emsp;Otoparkımızda birçok bankaya ait bankamatik bulunmaktadır.<br><br>' +
+    '&emsp;Hastanemiz Çanakkale-İzmir Karayoluüzerinde, Edremit’e 3.Km mesafededir.'
+    ,
+    '2-danma': 'ÖZEL EDREMİT KÖRFEZ HASTANESİ HAKKINDA<br><br>' +
+    '&emsp;Kuzey Ege Bölgesinde sağlık alanında tüm branşlarda (dahili ve cerrahi ) ' +
+    'hissedilen eksiklikler nedeniyle planlanıp bölgenin gereksinimlerine uygun olarak 2010 ' +
+    'yılında hizmet vermeye başlayan A sınıfı bir hastane olarak çevre il ve ilçelerde başta ' +
+    'olmak üzere Balıkesir Edremit Körfez Bölgesinin en modern ve en kapsamlı sağlık kuruluşlarından biridir.<br><br>' +
+    '&emsp;Yaklaşık 11 bin metrekare kapalı alana kurulu olan hastanemiz; 28’i yoğun bakım olmak üzere toplam 93 hasta yatağı, ' +
+    'tam teşekküllü genel yoğun bakım, kardiyovasküler cerrahi yoğun bakım, koroner yoğun bakım, cerrahi yoğun bakım ve ' +
+    'yenidoğan yoğun bakım servisleri ve modern ameliyathaneleri, doğumhaneleri, fizik tedavi ve rehabilitasyon servisi, ' +
+    'diş ağız ve çene cerrahisi ünitesi, modern radyoloji görüntüleme merkezi, tıbbi biyokimya ve mikrobiyoloji laboratuvarları' +
+    'ile hemen hemen tüm branşlarda SGK, anlaşmalı özel sigorta ve kurumlarla poliklinik, yatarak teşhis ve tedavi hizmeti sunmaktadır.<br><br>' +
+    '&emsp;Hastanemizde tüm hasta ve misafirlerimizin kullanabileceği ücretsiz otoparklarımız hizmet vermektedir.<br><br>' +
+    '&emsp;Otoparkımızda birçok bankaya ait bankamatik bulunmaktadır.<br><br>' +
+    '&emsp;Hastanemiz Çanakkale-İzmir Karayoluüzerinde, Edremit’e 3.Km mesafededir.'
+    ,
+    '3-bekleme-alan': 'ÖZEL EDREMİT KÖRFEZ HASTANESİ HAKKINDA<br><br>' +
+    '&emsp;Kuzey Ege Bölgesinde sağlık alanında tüm branşlarda (dahili ve cerrahi ) ' +
+    'hissedilen eksiklikler nedeniyle planlanıp bölgenin gereksinimlerine uygun olarak 2010 ' +
+    'yılında hizmet vermeye başlayan A sınıfı bir hastane olarak çevre il ve ilçelerde başta ' +
+    'olmak üzere Balıkesir Edremit Körfez Bölgesinin en modern ve en kapsamlı sağlık kuruluşlarından biridir.<br><br>' +
+    '&emsp;Yaklaşık 11 bin metrekare kapalı alana kurulu olan hastanemiz; 28’i yoğun bakım olmak üzere toplam 93 hasta yatağı, ' +
+    'tam teşekküllü genel yoğun bakım, kardiyovasküler cerrahi yoğun bakım, koroner yoğun bakım, cerrahi yoğun bakım ve ' +
+    'yenidoğan yoğun bakım servisleri ve modern ameliyathaneleri, doğumhaneleri, fizik tedavi ve rehabilitasyon servisi, ' +
+    'diş ağız ve çene cerrahisi ünitesi, modern radyoloji görüntüleme merkezi, tıbbi biyokimya ve mikrobiyoloji laboratuvarları' +
+    'ile hemen hemen tüm branşlarda SGK, anlaşmalı özel sigorta ve kurumlarla poliklinik, yatarak teşhis ve tedavi hizmeti sunmaktadır.<br><br>' +
+    '&emsp;Hastanemizde tüm hasta ve misafirlerimizin kullanabileceği ücretsiz otoparklarımız hizmet vermektedir.<br><br>' +
+    '&emsp;Otoparkımızda birçok bankaya ait bankamatik bulunmaktadır.<br><br>' +
+    '&emsp;Hastanemiz Çanakkale-İzmir Karayoluüzerinde, Edremit’e 3.Km mesafededir.'
+    ,
+    '4-kafeterya': 'ÖZEL EDREMİT KÖRFEZ HASTANESİ HAKKINDA<br><br>' +
+    '&emsp;Kuzey Ege Bölgesinde sağlık alanında tüm branşlarda (dahili ve cerrahi ) ' +
+    'hissedilen eksiklikler nedeniyle planlanıp bölgenin gereksinimlerine uygun olarak 2010 ' +
+    'yılında hizmet vermeye başlayan A sınıfı bir hastane olarak çevre il ve ilçelerde başta ' +
+    'olmak üzere Balıkesir Edremit Körfez Bölgesinin en modern ve en kapsamlı sağlık kuruluşlarından biridir.<br><br>' +
+    '&emsp;Yaklaşık 11 bin metrekare kapalı alana kurulu olan hastanemiz; 28’i yoğun bakım olmak üzere toplam 93 hasta yatağı, ' +
+    'tam teşekküllü genel yoğun bakım, kardiyovasküler cerrahi yoğun bakım, koroner yoğun bakım, cerrahi yoğun bakım ve ' +
+    'yenidoğan yoğun bakım servisleri ve modern ameliyathaneleri, doğumhaneleri, fizik tedavi ve rehabilitasyon servisi, ' +
+    'diş ağız ve çene cerrahisi ünitesi, modern radyoloji görüntüleme merkezi, tıbbi biyokimya ve mikrobiyoloji laboratuvarları' +
+    'ile hemen hemen tüm branşlarda SGK, anlaşmalı özel sigorta ve kurumlarla poliklinik, yatarak teşhis ve tedavi hizmeti sunmaktadır.<br><br>' +
+    '&emsp;Hastanemizde tüm hasta ve misafirlerimizin kullanabileceği ücretsiz otoparklarımız hizmet vermektedir.<br><br>' +
+    '&emsp;Otoparkımızda birçok bankaya ait bankamatik bulunmaktadır.<br><br>' +
+    '&emsp;Hastanemiz Çanakkale-İzmir Karayoluüzerinde, Edremit’e 3.Km mesafededir.'
+    ,
+    '5-acil-girii': '<br><br><br>Acil Servisimiz her türlü sağlık ihtiyacına zamanında ve etkin cevap verebilmek amacıyla yeterli sayıda uzman doktor ve yardımcı '+
+    'sağlık personeliyle 7 gün 24 saat hizmet vermektedir. 6 gözlem odası, 2 yataklı acil müdehale/girişim odası, triaj odası, 1 alçı '+
+    'odası ile hizmet veren acil serviste 24 saat CT, MRI, Röntgen ve tıbbi laboratuar tahlilleri yapılmaktadır.'
+    ,
+    '6-rntgen': '<br><br><br>Radyoloji görüntüleme merkezinde; Röntgen, Renkli Doppler Ultrasonografi, Digital Mamografi, Digital Floroskopi, Kemik Dansitometresi, '+
+    'Bilgisayarlı Tomografi(CT), 1,5 Tesla Manyetik Rezonans Görüntüleme(MRI) ileri teknolojik cihazlarla hastalıkların teşhisi için kullanılmakta ve '+
+    'PACS teknolojisiyle tüm hekimlerimizin hizmetine sunulmaktadır.'
+    ,
+    '7-ultrasonografi': '<br><br><br>Radyoloji görüntüleme merkezinde; Röntgen, Renkli Doppler Ultrasonografi, Digital Mamografi, Digital Floroskopi, Kemik Dansitometresi, '+
+    'Bilgisayarlı Tomografi(CT), 1,5 Tesla Manyetik Rezonans Görüntüleme(MRI) ileri teknolojik cihazlarla hastalıkların teşhisi için kullanılmakta ve '+
+    'PACS teknolojisiyle tüm hekimlerimizin hizmetine sunulmaktadır.'
+    ,
+    '8-mamografi': '<br><br><br>Radyoloji görüntüleme merkezinde; Röntgen, Renkli Doppler Ultrasonografi, Digital Mamografi, Digital Floroskopi, Kemik Dansitometresi, '+
+    'Bilgisayarlı Tomografi(CT), 1,5 Tesla Manyetik Rezonans Görüntüleme(MRI) ileri teknolojik cihazlarla hastalıkların teşhisi için kullanılmakta ve '+
+    'PACS teknolojisiyle tüm hekimlerimizin hizmetine sunulmaktadır.'
+    ,
+    '9-floroskopi': '<br><br><br>Radyoloji görüntüleme merkezinde; Röntgen, Renkli Doppler Ultrasonografi, Digital Mamografi, Digital Floroskopi, Kemik Dansitometresi, '+
+    'Bilgisayarlı Tomografi(CT), 1,5 Tesla Manyetik Rezonans Görüntüleme(MRI) ileri teknolojik cihazlarla hastalıkların teşhisi için kullanılmakta ve '+
+    'PACS teknolojisiyle tüm hekimlerimizin hizmetine sunulmaktadır.'
+    ,
+    '10-kemik-densitometresi': '<br><br><br>Radyoloji görüntüleme merkezinde; Röntgen, Renkli Doppler Ultrasonografi, Digital Mamografi, Digital Floroskopi, Kemik Dansitometresi, '+
+    'Bilgisayarlı Tomografi(CT), 1,5 Tesla Manyetik Rezonans Görüntüleme(MRI) ileri teknolojik cihazlarla hastalıkların teşhisi için kullanılmakta ve '+
+    'PACS teknolojisiyle tüm hekimlerimizin hizmetine sunulmaktadır.'
+    ,
+    '11-bilgisayarl-tomografi-ct': '<br><br><br>Radyoloji görüntüleme merkezinde; Röntgen, Renkli Doppler Ultrasonografi, Digital Mamografi, Digital Floroskopi, Kemik Dansitometresi, '+
+    'Bilgisayarlı Tomografi(CT), 1,5 Tesla Manyetik Rezonans Görüntüleme(MRI) ileri teknolojik cihazlarla hastalıkların teşhisi için kullanılmakta ve '+
+    'PACS teknolojisiyle tüm hekimlerimizin hizmetine sunulmaktadır.'
+    ,
+    '12-manyetin-rezonans-grntleme-mri': '<br><br><br>Radyoloji görüntüleme merkezinde; Röntgen, Renkli Doppler Ultrasonografi, Digital Mamografi, Digital Floroskopi, Kemik Dansitometresi, '+
+    'Bilgisayarlı Tomografi(CT), 1,5 Tesla Manyetik Rezonans Görüntüleme(MRI) ileri teknolojik cihazlarla hastalıkların teşhisi için kullanılmakta ve '+
+    'PACS teknolojisiyle tüm hekimlerimizin hizmetine sunulmaktadır.'
+    ,
+    '13-bokmya-ve-mkrobyoloj-laboratuvari': '<br><br><br>En ileri teknolojik cihazlar kullanılarak hastalardan alınan her türlü numunelerle hastalık teşhişlerine yönelik tetkikler en kısa '+
+    'sürede uluslararası kalite güvencesi kuralları uygulanarak neticelendirilmektedir.'
+    ,
+    '14-bokmya-ve-mkrobyoloj-laboratuvari-2': '<br><br><br>En ileri teknolojik cihazlar kullanılarak hastalardan alınan her türlü numunelerle hastalık teşhişlerine yönelik tetkikler en kısa '+
+    'sürede uluslararası kalite güvencesi kuralları uygulanarak neticelendirilmektedir.'
+    ,
+    '15-eswl-bbrek-tai-kirma-ntes': '<br><br><br>Kuzey Ege Bölgesinde büyük bir eksikliğin giderilmesi amacıyla kurulmuş ve Uzman Hekimlerimiz tarafından uygulaması yapılarak hizmete sunulmuştur. '
+    ,
+    '16-di-poliklinii': '<br><br><br>Bu ünitemizde 2 diş hekimi çene ve ağız cerrahi uzmanı ile Panoramik röntgen, Panoramik bilgisayarlı Tomografi ve diğer modern diş ekipmanlarıyla, ağız çene cerrahisi, '+
+    'tüm diş tedavileri, diş implantları ve protezleri yapılmaktadır.'
+    ,
+    '17-az-ve-ene-cerrahisi-poliklinii': '<br><br><br>Bu ünitemizde 2 diş hekimi çene ve ağız cerrahi uzmanı ile Panoramik röntgen, Panoramik bilgisayarlı Tomografi ve diğer modern diş ekipmanlarıyla, ağız çene cerrahisi, '+
+    'tüm diş tedavileri, diş implantları ve protezleri yapılmaktadır.'
+    ,
+    '18-panaromik-tomografi': '<br><br><br>Bu ünitemizde 2 diş hekimi çene ve ağız cerrahi uzmanı ile Panoramik röntgen, Panoramik bilgisayarlı Tomografi ve diğer modern diş ekipmanlarıyla, ağız çene cerrahisi, '+
+    'tüm diş tedavileri, diş implantları ve protezleri yapılmaktadır.'
+    ,
+    '25-kadn-doum-ve-hastalklar-poliklinii': '<br><br><br>Kadın Doğum ve Hastalıkları Polikliniği ( Dört boyutlu  General Elektrik Volusen E80 Renkli Doppler USG ile  gelişmiş uygulama ve üstün görüntü kalitesiyle '+
+    'tüm hamilelik süreci boyunca takibi sağlar, günlük rutin işlemlerden karmaşık işlemlere kadar oluşabilecek problemlere mümkün olduğunca erken müdahale edebilmek için teşhis ve erken tedaviye olanak '+
+    'sunan dünyadaki en ileri teknolojiyi kullanmaktadır).'
+    ,
+    '26-kadn-doum-ve-hastalklar-poliklinii-2': '<br><br><br>Kadın Doğum ve Hastalıkları Polikliniği ( Dört boyutlu  General Elektrik Volusen E80 Renkli Doppler USG ile  gelişmiş uygulama ve üstün görüntü kalitesiyle '+
+    'tüm hamilelik süreci boyunca takibi sağlar, günlük rutin işlemlerden karmaşık işlemlere kadar oluşabilecek problemlere mümkün olduğunca erken müdahale edebilmek için teşhis ve erken tedaviye olanak '+
+    'sunan dünyadaki en ileri teknolojiyi kullanmaktadır).'
+    ,
+    '27-gz-poliklinii': '<br><br><br>Göz poliklinik odaları (Göz hastalıklarının teşhis ve tedavisi amacıyla anjiografi, bilgisayarlı göz tomografisi (Optik Koherans Tomografi) dahil olmak üzere ileri teknolojik cihazlarla '+
+    'donatılmış olup 3 uzman hekimimiz tarafından hizmet verilmekte ve her türlü göz hastalıklarına yönelik operasyonlar hastanemizde gerçekleştirilmektedir). Optik Koherans Tomografi düşük enerjili laser ışınları '+
+    'kullanılarak göze dokunmadan ve radyasyona maruz bırakılmadan zarar vermeden yapılan gözün retina tabakası ve görme sinirinin taranmasıyla retina, sarı nokta, glokom gibi birçok hastalığın teşhis ve takibinde '+
+    'kullanılan güvenilir bir tetkiktir.'
+    ,
+    '28-gz-poliklinii-2': '<br><br><br>Göz poliklinik odaları (Göz hastalıklarının teşhis ve tedavisi amacıyla anjiografi, bilgisayarlı göz tomografisi (Optik Koherans Tomografi) dahil olmak üzere ileri teknolojik cihazlarla '+
+    'donatılmış olup 3 uzman hekimimiz tarafından hizmet verilmekte ve her türlü göz hastalıklarına yönelik operasyonlar hastanemizde gerçekleştirilmektedir). Optik Koherans Tomografi düşük enerjili laser ışınları '+
+    'kullanılarak göze dokunmadan ve radyasyona maruz bırakılmadan zarar vermeden yapılan gözün retina tabakası ve görme sinirinin taranmasıyla retina, sarı nokta, glokom gibi birçok hastalığın teşhis ve takibinde '+
+    'kullanılan güvenilir bir tetkiktir.'
+    ,
+    '29-gz-poliklinii-3': '<br><br><br>Göz poliklinik odaları (Göz hastalıklarının teşhis ve tedavisi amacıyla anjiografi, bilgisayarlı göz tomografisi (Optik Koherans Tomografi) dahil olmak üzere ileri teknolojik cihazlarla '+
+    'donatılmış olup 3 uzman hekimimiz tarafından hizmet verilmekte ve her türlü göz hastalıklarına yönelik operasyonlar hastanemizde gerçekleştirilmektedir). Optik Koherans Tomografi düşük enerjili laser ışınları '+
+    'kullanılarak göze dokunmadan ve radyasyona maruz bırakılmadan zarar vermeden yapılan gözün retina tabakası ve görme sinirinin taranmasıyla retina, sarı nokta, glokom gibi birçok hastalığın teşhis ve takibinde '+
+    'kullanılan güvenilir bir tetkiktir.'
+    ,
+    '30-gz-poliklinii-4': '<br><br><br>Göz poliklinik odaları (Göz hastalıklarının teşhis ve tedavisi amacıyla anjiografi, bilgisayarlı göz tomografisi (Optik Koherans Tomografi) dahil olmak üzere ileri teknolojik cihazlarla '+
+    'donatılmış olup 3 uzman hekimimiz tarafından hizmet verilmekte ve her türlü göz hastalıklarına yönelik operasyonlar hastanemizde gerçekleştirilmektedir). Optik Koherans Tomografi düşük enerjili laser ışınları '+
+    'kullanılarak göze dokunmadan ve radyasyona maruz bırakılmadan zarar vermeden yapılan gözün retina tabakası ve görme sinirinin taranmasıyla retina, sarı nokta, glokom gibi birçok hastalığın teşhis ve takibinde '+
+    'kullanılan güvenilir bir tetkiktir.'
+    ,
+    '32-doumhane': '<br><br><br>Hastanemizde 2 modern doğum odası mevcuttur.'
+    ,
+    '43-fizik-tedavi-ve-rehabilitasyon': '<br><br><br>Fizik Tedavi ve Rehabilitasyon Servisi; En iyi koşullarda hizmet vermek amacıyla ileri teknolojik cihazlarla donatılarak hastaların günlük yaşam aktivitelerini geri '+
+    'kazanmaları, çalışma ve sosyal yaşamlarına etkin bir şekilde devam edebilmeleri için hastaya özel yaklaşımlarla, randevu sistemi uygulanarak, omurilik yaralanmaları, inme (beden felci), kafa ya da diğer '+
+    'vücut travmaları sonrası oluşan yaralanmalar, spor yaralanmaları, romatolojik hastalıklar, multiple sklerozis dahil olmak üzere diğer nörolojik kas ve sinir sistemi hastalıkları, operasyon öncesi ve sonrası '+
+    'ortopedik rehabilitasyonlar uzman hekimlerimiz ve fizyoterapistlerimiz tarafından tedavi edilmektedirler.'
+    ,
+    '44-fizik-tedavi-ve-rehabilitasyon-2': '<br><br><br>Fizik Tedavi ve Rehabilitasyon Servisi; En iyi koşullarda hizmet vermek amacıyla ileri teknolojik cihazlarla donatılarak hastaların günlük yaşam aktivitelerini geri '+
+    'kazanmaları, çalışma ve sosyal yaşamlarına etkin bir şekilde devam edebilmeleri için hastaya özel yaklaşımlarla, randevu sistemi uygulanarak, omurilik yaralanmaları, inme (beden felci), kafa ya da diğer '+
+    'vücut travmaları sonrası oluşan yaralanmalar, spor yaralanmaları, romatolojik hastalıklar, multiple sklerozis dahil olmak üzere diğer nörolojik kas ve sinir sistemi hastalıkları, operasyon öncesi ve sonrası '+
+    'ortopedik rehabilitasyonlar uzman hekimlerimiz ve fizyoterapistlerimiz tarafından tedavi edilmektedirler.'
+    ,
+    '45-fizik-tedavi-ve-rehabilitasyon-3': '<br><br><br>Fizik Tedavi ve Rehabilitasyon Servisi; En iyi koşullarda hizmet vermek amacıyla ileri teknolojik cihazlarla donatılarak hastaların günlük yaşam aktivitelerini geri '+
+    'kazanmaları, çalışma ve sosyal yaşamlarına etkin bir şekilde devam edebilmeleri için hastaya özel yaklaşımlarla, randevu sistemi uygulanarak, omurilik yaralanmaları, inme (beden felci), kafa ya da diğer '+
+    'vücut travmaları sonrası oluşan yaralanmalar, spor yaralanmaları, romatolojik hastalıklar, multiple sklerozis dahil olmak üzere diğer nörolojik kas ve sinir sistemi hastalıkları, operasyon öncesi ve sonrası '+
+    'ortopedik rehabilitasyonlar uzman hekimlerimiz ve fizyoterapistlerimiz tarafından tedavi edilmektedirler.'
+    ,
+    '46-fizik-tedavi-ve-rehabilitasyon-odas': '<br><br><br>Fizik Tedavi ve Rehabilitasyon Servisi; En iyi koşullarda hizmet vermek amacıyla ileri teknolojik cihazlarla donatılarak hastaların günlük yaşam aktivitelerini geri '+
+    'kazanmaları, çalışma ve sosyal yaşamlarına etkin bir şekilde devam edebilmeleri için hastaya özel yaklaşımlarla, randevu sistemi uygulanarak, omurilik yaralanmaları, inme (beden felci), kafa ya da diğer '+
+    'vücut travmaları sonrası oluşan yaralanmalar, spor yaralanmaları, romatolojik hastalıklar, multiple sklerozis dahil olmak üzere diğer nörolojik kas ve sinir sistemi hastalıkları, operasyon öncesi ve sonrası '+
+    'ortopedik rehabilitasyonlar uzman hekimlerimiz ve fizyoterapistlerimiz tarafından tedavi edilmektedirler.'
+    ,
+    '49-kardiovaskler-cerrahi-ameliyathanesi': '<br><br><br>Birisi Kardiovasküler Cerrahi Ameliyathanesi olmak üzere dört ameliyathane mevcuttur.'
+    ,
+    '51-koroner-youn-bakm': '<br><br><br>4 Yatak Kapesiteli Koroner Yoğun Bakım Servisi'
+    ,
+    '52-genel-cerrahi-youn-bakm': '<br><br><br>4 Yatak Kapesiteli Genel Cerrahi Yoğun Bakım Servisi'
+    ,
+    '53-kardiovaskler-cerrahi-youn-bakm': '<br><br><br>4 Yatak Kapesiteli Kardiovasküler Cerrahi Yoğun Bakım Servisi'
+    ,
+    '54-eeg-emg-uyku-laboratuvar-odas': '<br><br><br>EMG,EEG,UYKU Laboratuvar Odası ;Epilepsi, kas ve sinir hastalıkları ve uyku bozukluklarının teşhisin de kullanılmak üzere hizmet vermektedir.'
+    ,
+    '55-gnbirlik-hasta-tedavi-ve-takip-servisi': '<br><br><br>Günübirlik hasta tedavi ve takip servisi; bu serviste 24 saate kadar varan sürelerle tedavi ve takip edilen hastalarımıza hizmet verilmektedir.'
+    ,
+    '56-gnbirlik-hasta-tedavi-ve-takip-servisi-2': '<br><br><br>Günübirlik hasta tedavi ve takip servisi; bu serviste 24 saate kadar varan sürelerle tedavi ve takip edilen hastalarımıza hizmet verilmektedir.'
   };
 
   function infoEvent() {
